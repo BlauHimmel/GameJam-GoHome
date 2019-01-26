420c9f514fa1c1301f9bdad438d34859906e1298
周宇明 <461310150@qq.com>
2019-01-26 16:34:10 +0800

    Add startup scene.

---
+++ Assets/Scripts/StartupUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class StartupUI : MonoBehaviour
+{
+    private Button m_StartButton;
+    private Button m_ExitButton;
+
+    void Start ()
+    {
+        m_StartButton = GameObject.Find("StartButton").GetComponentInChildren<Button>();
+        m_ExitButton = GameObject.Find("ExitButton").GetComponentInChildren<Button>();
+
+        m_StartButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("MainScene");
+        });
+
+        m_ExitButton.onClick.AddListener(() =>
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        });
+    }
+}
